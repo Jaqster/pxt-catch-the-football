b6f90eb7eaa5063201d4de886d9ffb28bbf2f790
Jacqueline Russell <jacqueline.russell@microsoft.com>
2019-10-31 16:39:24 -0700

    Auto-initialized.

---
+++ main.ts
@@ -0,0 +1,45 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    music.baDing.play()
+    info.changeScoreBy(1)
+    football.setPosition(Math.randomRange(0, 160), Math.randomRange(0, 120))
+    info.startCountdown(10)
+})
+let football: Sprite = null
+scene.setBackgroundColor(7)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . a a a a a a . . .
+    . . . . . . a a 4 4 4 4 a a . .
+    . . . . . . a 4 4 4 4 4 4 a a .
+    . . . . . . a 4 4 6 4 6 4 4 a .
+    . . . . . . a 4 4 4 4 4 4 4 a .
+    . . 4 . . . a 4 4 4 3 3 3 a a .
+    . . 4 4 4 4 . a a a a a a a . .
+    . . . . . 4 4 4 4 4 4 4 a a 4 4
+    . . . . . . . 4 4 4 4 4 4 4 4 .
+    . . . . . . 4 4 4 4 4 4 4 . . .
+    . . . . . . 4 4 4 4 4 4 . . . .
+    . . . . 4 4 4 . 4 4 . . . . . .
+    . . . 4 4 . . . . 4 4 . . . . .
+    . a a a . . . . . a a a . . . .
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+football = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . e e e e e e . . . . .
+    . . . e e e e e e e e e e . . .
+    . . e e e e e e e e e e e e . .
+    . e e e e e e e e e e e e e e .
+    e e e e e e e e e e e e e e e e
+    e e 1 1 1 e e 1 e e 1 1 1 e e e
+    e e e e e e e e e e e e e e e e
+    . . e e e e e e e e e e e e e .
+    . . . e e e e e e e e e e . . .
+    . . . . e e e e e e e . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Projectile)
